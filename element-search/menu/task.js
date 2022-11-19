488c9f67d543b0dc9fac8b2132d5cf0a57ce9d93
Maksim Lazarev <felshines@gmail.com>
2022-11-19 12:46:15 +0300

    homework-2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,13 +2,16 @@ let menuList = [...document.querySelectorAll('.menu_sub')];
 let menuWindow = document.querySelector(".menu");
 
 menuWindow.addEventListener('click', e => {
-  if(e.target.closest(".menu__item").querySelector(".menu_sub")){
-    e.preventDefault();
-    if(e.target.closest(".menu__item").querySelector(".menu_active")) {
-      e.target.closest(".menu__item").querySelector(".menu_sub").classList.remove("menu_active");
-    }else{
-      menuList.forEach(elMenu => elMenu.classList.remove("menu_active"));
-      e.target.closest(".menu__item").querySelector(".menu_sub").classList.add("menu_active");
+  let menu = e.target.closest(".menu__item");
+  if(menu) {
+    if (menu.querySelector(".menu_sub")) {
+      e.preventDefault();
+      if (menu.querySelector(".menu_active")) {
+        menu.querySelector(".menu_sub").classList.remove("menu_active");
+      } else {
+        menuList.forEach(elMenu => elMenu.classList.remove("menu_active"));
+        menu.querySelector(".menu_sub").classList.add("menu_active");
+      }
     }
   }
 });
