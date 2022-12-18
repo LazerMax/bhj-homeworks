8a8082f5bed7a1cf7b05b1f89fa89a740ee7f178
Maksim Lazarev <felshines@gmail.com>
2022-12-18 21:18:49 +0300

    homework-6

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,42 @@
+const xhr = new XMLHttpRequest();
+const items = document.querySelector("#items");
+
+window.onload = () => {
+
+    if (localStorage.getItem("itemsArray")) {
+        let myObj = JSON.parse(localStorage.getItem("itemsArray"));
+        for (key in myObj) {
+            let template = `<div class="item oldElement"><div class="item__code">${key}</div>
+                <div class="item__value">${myObj[key]}</div>
+               <div class="item__currency">руб.</div></div>`;
+            items.insertAdjacentHTML("afterEnd", template);
+        }
+        document.querySelector(".loader_active").classList.remove("loader_active");
+    }
+}
+
+xhr.addEventListener("readystatechange", () => {
+    if (xhr.readyState === xhr.DONE) {
+        let myObj = JSON.parse(xhr.responseText).response.Valute;
+        let newObj = {};
+
+        for (key in myObj) {
+            let template = `<div class="item"><div class="item__code">${key}</div>
+                <div class="item__value">${myObj[key].Value}</div>
+               <div class="item__currency">руб.</div></div>`;
+            items.insertAdjacentHTML("afterEnd", template);
+            newObj[key] = myObj[key].Value;
+        }
+        if (!(localStorage.getItem("itemsArray"))){
+            document.querySelector(".loader_active").classList.remove("loader_active");
+        } else{
+            $('.oldElement').remove();
+        }
+
+        localStorage.setItem("itemsArray", JSON.stringify(newObj));
+    }
+    });
+
+xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/slow-get-courses");
+xhr.send();
+
