0e1cd037875a4be4d5709df55cf9227d02d1c10b
Maksim Lazarev <felshines@gmail.com>
2022-12-18 14:19:57 +0300

    homework-5

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,55 +3,55 @@ const btnAdd = document.querySelector(".tasks__add");
 
 
 
+window.onload = () => {
+    if (localStorage.getItem("tasksArray")) {
+        let masString = JSON.parse(localStorage.getItem("tasksArray"));
+        masString.forEach(e => {
 
-    window.onload = () => {
-    if (localStorage.length != 0) {
-        for (let i = 0; i < localStorage.length; i++) {
-            let key = localStorage.key(i);
-            let template = `${localStorage.getItem(key)}`;
-            taskList.insertAdjacentHTML('afterbegin', template)
-        }
+            let template = `<div data-id="${++localStorage.length}" class="task">
+                <div class="task__title">${e}</div>
+                <a href="#" class="task__remove">&times;</a></div>`;
+
+            taskList.insertAdjacentHTML("afterEnd", template);
+        })
     }
-
-        let btnsRemove = document.querySelectorAll(".task__remove");
-
-        if(btnsRemove){
-            btnsRemove.forEach(e => {
-                    e.addEventListener("click", el => {
-                        let removeElement = el.target.closest(".task");
-                        removeElement.remove();
-                        console.log(removeElement.dataset.id);
-                        localStorage.removeItem(`${removeElement.dataset.id}`)
-                    });
-                });
-        }
-
-    }
+}
 
 
 btnAdd.addEventListener("click",  el => {
 
-            let task = document.getElementById("task__input").value.trim();
+    let task = document.getElementById("task__input").value.trim();
 
-            if (task != "") {
-
-                let template = `<div data-id="${++localStorage.length}" class="task">
+    if (task != "") {
+         let template = `<div data-id="${++localStorage.length}" class="task">
                 <div class="task__title">${task}</div>
                 <a href="#" class="task__remove">&times;</a></div>`;
+         taskList.insertAdjacentHTML("afterEnd", template);
 
-                taskList.insertAdjacentHTML("afterEnd", template);
-                localStorage.setItem(`${++localStorage.length}`, template);
+        let tasks = document.querySelectorAll(".task__title");
+        let masString = [];
+        tasks.forEach(elem => {masString.push(elem.textContent)});
 
-               document.getElementById("task__input").value = "";
-            }
-            let btnRemove = document.querySelector(".task__remove");
-            btnRemove.addEventListener("click", el => {
+         localStorage.setItem("tasksArray", JSON.stringify(masString));
+
+         document.getElementById("task__input").value = "";
+    }
+
+    let btnRemove = document.querySelector(".task__remove");
+
+        if (btnRemove){
+        btnRemove.addEventListener("click", el => {
             let removeElement = el.target.closest(".task");
             removeElement.remove();
-    });
+
+            let tasks = document.querySelectorAll(".task__title");
+            let masString = [];
+            tasks.forEach(elem => {masString.push(elem.textContent)});
+            localStorage.setItem("tasksArray", JSON.stringify(masString));
+
+        });
+    }
 });
-
-
 
 
 
